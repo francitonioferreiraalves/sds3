c80732cbd02f1d7df7f2ea0aeaadba438f969493
francitonioferreiraalves <francitonio@gmail.com>
2021-05-06 14:23:29 -0300

    Domain model, database seed

---
+++ dsvendas/src/test/java/com/tito/sds3/Sds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tito.sds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
